e9de1e842fb0aefb794edaaf0d1cfe309d3231f5
SEF-3070 <josef.weikert@gmail.com>
2025-04-07 11:31:13 +0100

    netlify-build

--- src/components/nav/SettingsDropdown.tsx
+++ src/components/nav/SettingsDropdown.tsx
@@ -1,11 +1,4 @@
-import {
-  DropdownMenu,
-  Dialog,
-  Button,
-  Flex,
-  Text,
-  TextField,
-} from "@radix-ui/themes";
+import { DropdownMenu, Button } from "@radix-ui/themes";
 import { GearIcon } from "@radix-ui/react-icons";
 import { AccentColors } from "../../App";
 import { Dispatch } from "react";
@@ -19,52 +12,6 @@ function SettingsDropdown({ setAccentColor }: Props) {
     setAccentColor(color);
   }
 
-  function handleDelete() {
-    <Dialog.Root>
-      <Dialog.Trigger>
-        <Button>Edit profile</Button>
-      </Dialog.Trigger>
-
-      <Dialog.Content maxWidth="450px">
-        <Dialog.Title>Edit profile</Dialog.Title>
-        <Dialog.Description size="2" mb="4">
-          Make changes to your profile.
-        </Dialog.Description>
-
-        <Flex direction="column" gap="3">
-          <label>
-            <Text as="div" size="2" mb="1" weight="bold">
-              Name
-            </Text>
-            <TextField.Root
-              defaultValue="Freja Johnsen"
-              placeholder="Enter your full name"
-            />
-          </label>
-          <label>
-            <Text as="div" size="2" mb="1" weight="bold">
-              Email
-            </Text>
-            <TextField.Root
-              defaultValue="freja@example.com"
-              placeholder="Enter your email"
-            />
-          </label>
-        </Flex>
-
-        <Flex gap="3" mt="4" justify="end">
-          <Dialog.Close>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </Dialog.Close>
-          <Dialog.Close>
-            <Button>Save</Button>
-          </Dialog.Close>
-        </Flex>
-      </Dialog.Content>
-    </Dialog.Root>;
-  }
   return (
     <DropdownMenu.Root modal={false}>
       <DropdownMenu.Trigger>
@@ -74,11 +21,6 @@ function SettingsDropdown({ setAccentColor }: Props) {
         </Button>
       </DropdownMenu.Trigger>
       <DropdownMenu.Content>
-        {/* <DropdownMenu.Item shortcut="⌘ S">Save</DropdownMenu.Item>
-        <DropdownMenu.Item shortcut="⌘ D">Duplicate</DropdownMenu.Item>
-        <DropdownMenu.Separator />
-        <DropdownMenu.Item shortcut="⌘ N">Archive</DropdownMenu.Item> */}
-
         <DropdownMenu.Sub>
           <DropdownMenu.SubTrigger>Theme</DropdownMenu.SubTrigger>
           <DropdownMenu.SubContent>
@@ -93,14 +35,6 @@ function SettingsDropdown({ setAccentColor }: Props) {
             </DropdownMenu.Item>
           </DropdownMenu.SubContent>
         </DropdownMenu.Sub>
-
-        {/* <DropdownMenu.Separator /> */}
-        {/* <DropdownMenu.Item>Share</DropdownMenu.Item>
-        <DropdownMenu.Item>Add to favorites</DropdownMenu.Item>
-        <DropdownMenu.Separator />
-        <DropdownMenu.Item shortcut="⌘ ⌫" color="red" onClick={handleDelete}>
-          Delete
-        </DropdownMenu.Item> */}
       </DropdownMenu.Content>
     </DropdownMenu.Root>
   );
